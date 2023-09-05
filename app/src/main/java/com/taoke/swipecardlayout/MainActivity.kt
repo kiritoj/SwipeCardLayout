7de50553be93c3def93961e3640bfcac7a46b9a9
sakura <2552744904@qq.com>
2023-09-05 23:49:07 +0800

    项目初始化

---
+++ app/src/main/java/com/taoke/swipecardlayout/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.taoke.swipecardlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
